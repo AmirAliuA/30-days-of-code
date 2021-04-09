3e5ca46d0805464b6878eb5c10b27dba7e8c115d
Amir Aliu <amiraliudev@gmail.com>
2021-04-09 03:03:33 +0200

    Create hello-world.cs

---
+++ hello-world.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Solution {
+    static void Main(String[] args) {
+        String inputString = Console.ReadLine();
+
+        Console.WriteLine("Hello, World.");
+        Console.WriteLine(inputString);
+    }
+}
