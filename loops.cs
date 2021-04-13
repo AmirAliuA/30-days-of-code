8c5221f955605ce87c351fad18ff303a641c2880
Amir Aliu <amiraliudev@gmail.com>
2021-04-13 13:22:22 +0200

    create loops.cs

---
+++ loops.cs
@@ -0,0 +1,9 @@
+class Solution {
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        for (int i = 1; i < 11; i++) {
+            Console.WriteLine(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
