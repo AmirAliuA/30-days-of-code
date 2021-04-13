79db873da3ccc99fb5082ecafe5ea899872ec705
Amir Aliu <amiraliudev@gmail.com>
2021-04-13 13:29:18 +0200

    update operators.cs

--- operators.cs
+++ operators.cs
@@ -1,1 +1,13 @@
-
+class Solution {
+    static void Main(string[] args) {
+        double meal_cost = Convert.ToDouble(Console.ReadLine());
+        int tip_percent = Convert.ToInt32(Console.ReadLine());
+        int tax_percent = Convert.ToInt32(Console.ReadLine());
+            
+        double tip = meal_cost * tip_percent / 100;
+        double tax = meal_cost * tax_percent / 100;
+        double totalCost = meal_cost + tip + tax;
+            
+        Console.WriteLine(Math.Round(totalCost));
+    }
+}
