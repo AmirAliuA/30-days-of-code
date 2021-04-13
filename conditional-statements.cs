0b93b6208a0e3832e371f0b3fe3e5ab09247b104
Amir Aliu <amiraliudev@gmail.com>
2021-04-13 13:20:41 +0200

    added the code

--- conditional-statements.cs
+++ conditional-statements.cs
@@ -1,1 +1,15 @@
-
+class Solution {
+   static void Main(String[] args) {
+        int N = Convert.ToInt32(Console.ReadLine());
+        
+        if (N % 2 == 1) {
+            Console.WriteLine("Weird");
+        } else if (N % 2 == 0 && N > 2 && N < 5) {
+            Console.WriteLine("Not Weird");
+        } else if (N % 2 == 0 && N > 6 && N <= 20) {
+            Console.WriteLine("Weird");
+        } else if (N % 2 == 0 && N > 20) {
+            Console.WriteLine("Not Weird");
+        }      
+    }
+}
